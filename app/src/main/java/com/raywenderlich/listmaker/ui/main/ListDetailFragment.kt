46672aad6493e950d322581d00d66e335e995e4b
Boondoggle <email.cfranco@gmail.com>
2021-09-05 23:53:25 -0400

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/listmaker/ui/main/ListDetailFragment.kt
@@ -0,0 +1,63 @@
+package com.raywenderlich.listmaker.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.preference.PreferenceManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.raywenderlich.listmaker.MainActivity
+import com.raywenderlich.listmaker.TaskList
+import com.raywenderlich.listmaker.databinding.ListDetailFragmentBinding
+
+
+class ListDetailFragment : Fragment() {
+
+    lateinit var binding: ListDetailFragmentBinding
+
+    companion object {
+        fun newInstance() = ListDetailFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = ListDetailFragmentBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(
+            requireActivity(),
+            MainViewModelFactory(PreferenceManager.getDefaultSharedPreferences(requireActivity()))
+        )
+            .get(MainViewModel::class.java)
+
+
+        val list: TaskList? =
+            arguments?.getParcelable(MainActivity.INTENT_LIST_KEY)
+        if (list != null) {
+            viewModel.list = list
+            requireActivity().title = list.name
+        }
+
+        binding.listItemsRecyclerview.adapter = ListItemRecyclerViewAdapter(viewModel.list)
+        binding.listItemsRecyclerview.layoutManager = LinearLayoutManager(requireContext())
+
+        viewModel.onTaskAdded = {
+            var adapter = binding.listItemsRecyclerview.adapter as ListItemRecyclerViewAdapter
+        adapter.tasksUpdated()
+        }
+
+
+
+    }
+
+}
